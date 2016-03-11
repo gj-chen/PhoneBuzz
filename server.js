087f3449cdb99592ec94a48e69e204f886050247
Gloria <gjchen@ucdavis.edu>
2016-03-10 21:19:02 -0800

    added gather function

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ var app = express();
 var server = http.createServer(function(req, res){
 	//Create a TwiML response 
 	var resp = new twilio.TwimlResponse(); 
-	//The TwiML response object will have function on it that correspond 
-	//to TwiML verbs and nouns. This example uses "Say" verb
-	//Passing in a string argument sets the content of the XML tag 
-	//Passing in an object literal set attributes on the XML tag 
-	resp.say({voice: 'woman'}, 'hello!! This is Gloria. I am testing Twilio and Node.js!');
+	 
+
+	resp.say({voice: 'woman'}, 'hello!! This is Gloria. I am testing Twilio and Node.js!')
+    	.gather({
+        	finishOnKey: '#'
+    	}, function() {
+        	this.say('Please enter a number');
+    });
+
+	console.log(resp.toString());
 
 	//Render the TwiML document using 'toString' 
 	res.writeHead(200, {
