ca1e3b04645e3707aaa739887c7c71f9f690ce53
Gloria <gjchen@ucdavis.edu>
2016-03-11 19:04:32 -0800

    made no progress today

--- newserver.js
+++ newserver.js
@@ -17,6 +17,7 @@ var router = express.Router();
 app.get('/', function(req, res){
  	//Create an object which will generate a capability token 
  	//Replace these two arguments with own account SID/auth token
+ 	//res.sendFile('/Users/gloriachen/Desktop/PhoneBuzz/index/index.html');
  	var capability = new twilio.Capability(
  		//process.env.TWILIO_ACCOUNT_SID,
  		'ACa1d489ae50b6b27532f10084df4310e7',
@@ -25,7 +26,6 @@ app.get('/', function(req, res){
  	);
  
  	//Give the capability generator permission to acccept incoming 
- 	//calls to ID 'gloria'
  	capability.allowClientIncoming(process.env.TWILIO_ACCOUNT_SID); 
  
  	//Render an HTML page which contains our capability token 
@@ -34,6 +34,10 @@ app.get('/', function(req, res){
  	}); 
  });//.listen(process.env.PORT || 5000);
 
+
+//app.get('/', function(request, response){
+  //  response.sendfile('yourhtmlpagename.html');
+//});
  
 
 /*app.post('/', function(req, res){
@@ -56,7 +60,7 @@ app.get('/', function(req, res){
 	}); 
 	res.send(twiml.toString());
 	res.end(resp.toString());
-});//.listen(process.env.PORT || 5000); 
+})//.listen(process.env.PORT || 5000); 
 */
 
 //Nothing is changed here 
@@ -72,7 +76,7 @@ var server = http.createServer(function(req, res){
 		}, function(){
 			this.say('Please enter a number and press the star key when complete. You have 20 seconds.');
 		}); 
-    console.log("hi gloria youre inside the post function");
+    //console.log("hi gloria youre inside the post function");
     res.writeHead(200, {
 		'Content-Type': 'text/xml'
 	});
@@ -89,7 +93,7 @@ var server = http.createServer(function(req, res){
     	}, function() {
         	this.say('Please enter a number and press the star key when complete. You have 20 seconds.');
     	});
-*/
+
 	//Render the TwiML document using 'toString' 
 	/*res.writeHead(200, {
 		'Content-Type': 'text/xml'
