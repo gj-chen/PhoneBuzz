e3c7dbc0ad6472cc705d1d7a5cfd36a76ff8cc34
Gloria <gjchen@ucdavis.edu>
2016-03-11 18:38:00 -0800

    using http server again

--- newserver.js
+++ newserver.js
@@ -32,21 +32,9 @@ app.get('/', function(req, res){
  	res.render('index.ejs',{
  		token:capability.generate()
  	}); 
- }).listen(process.env.PORT || 5000);
+ });//.listen(process.env.PORT || 5000);
 
-app.post('/', function(req, res){
-	var twilio = new twilio.TwimlResponse(); 
-
-	twilio.say({voice: 'woman'}, 'Gloria is testing Twilio and Node.js')
-		.gather({
-			finishOnKey: '*', 
-			timeout: '20' 
-		}, function(){
-			this.say('Please enter a number and press the star key when complete. You have 20 seconds.');
-		}); 
-    console.log("hi gloria youre inside the post function");
-    res.send(twiml.toString());
-}); 
+ 
 
 /*app.post('/', function(req, res){
 	var resp = new twilio.TwimlResponse(); 
@@ -73,9 +61,21 @@ app.post('/', function(req, res){
 
 //Nothing is changed here 
 //Create an HTTP server that renders TwiML 
-/*var server = http.createServer(function(req, res){
+var server = http.createServer(function(req, res){
 	//Create a TwiML response 
-	var resp = new twilio.TwimlResponse(); 
+	var twilio = new twilio.TwimlResponse(); 
+
+	twilio.say({voice: 'woman'}, 'Gloria is testing Twilio and Node.js')
+		.gather({
+			finishOnKey: '*', 
+			timeout: '20' 
+		}, function(){
+			this.say('Please enter a number and press the star key when complete. You have 20 seconds.');
+		}); 
+    console.log("hi gloria youre inside the post function");
+    res.send(twiml.toString());
+
+	/*var resp = new twilio.TwimlResponse(); 
 	 
 	resp.say({voice: 'woman'}, 'Gloria testing Twilio and Node.js')
     	.gather({
@@ -86,13 +86,13 @@ app.post('/', function(req, res){
     	}, function() {
         	this.say('Please enter a number and press the star key when complete. You have 20 seconds.');
     	});
-
+*/
 	//Render the TwiML document using 'toString' 
-	res.writeHead(200, {
+	/*res.writeHead(200, {
 		'Content-Type': 'text/xml'
 	}); 
-	res.end(resp.toString()); 
+	res.end(resp.toString()); */
 }).listen(process.env.PORT || 5000);
 
-*/
+
 console.log('Added the calling feature to my application!'); 
