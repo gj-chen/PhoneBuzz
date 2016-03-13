ec65ded5e9709bb3fd0315b8bc528e36e042d801
Gloria <gjchen@ucdavis.edu>
2016-03-12 21:24:48 -0800

    added space inside loop

--- newserver.js
+++ newserver.js
@@ -63,13 +63,11 @@ router.get('/fizzbuzz', function(req, res, next) {
 	var result;
 	console.log("before loop"); 
 	for (var i = 1; i <= digit_entered; i++) { 
+        result = "";
         if (i%3 == 0) result += 'fizz ';        
         else if (i%5 == 0) result += "buzz ";
         else if (i%3 == 0 && i%5 == 0) result+= "Fizzbuzz "; 
-        else{
-        	result += i;
-        	result += " "; 
-        } 
+        else result += i; 
 	}
 	
 	resp.say(result);
