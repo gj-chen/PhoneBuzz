84a03ffc7129b9d355db5a0fd69e7aa9ee6d3df5
Gloria <gjchen@ucdavis.edu>
2016-03-12 21:02:28 -0800

    toString

--- newserver.js
+++ newserver.js
@@ -62,13 +62,14 @@ router.get('/fizzbuzz', function(req, res, next) {
 	
 	var result = ""; 
 	for (var i = 1; i <= digit_entered; i++) { 
-        if (i%3 == 0) resp.say("fizz");      
-        else if (i%5 == 0) resp.say("buzz");
-        else if (i%3 == 0 && i%5 == 0) resp.say("hello"); 
-        else result resp.say(i);
+        if (i%3 == 0) result += 'fizz';        
+        else if (i%5 == 0) result += "buzz ";
+        else if (i%3 == 0 && i%5 == 0) result+= " Fizzbuzz "; 
+        else result += i; 
 	}
 	
-	resp.say(result);
+	resultString = resp.toString(); 
+	resp.say(resultString);
 
 	console.log(resp.toString());
 	
